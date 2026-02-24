4e0d92edb1a19e4ee57b02df7116c1f8e6ce2a0d
lmarusich <lmarusich@users.noreply.github.com>
2026-02-24 16:28:10 -0600

    update documentation

--- R/rateR.R
+++ R/rateR.R
@@ -1,6 +1,6 @@
-#' A shiny application for 
+#' A shiny application for human coding and rating tasks.
 #'
-#' rateR provides an intuitive interface with options to 
+#' rateR provides an intuitive interface with options to import and filter data, choose variables to code/rate, set constraints on the rating options, and save the rated data to a new file.
 #' @import shiny
 #' @export
 #' @seealso Web version: \url{https://lmarusich.shinyapps.io/rateR/}
